56a32ed729630c6a9d4e89c1c362cecbadc89a7d
zyxkad <zyxkad@gmail.com>
2025-11-25 20:02:49 -0700

    upgraded stuffs

--- common/src/main/java/com/github/litermc/vtil/mixin/valkyrienskies/MixinShipObjectServer.java
+++ common/src/main/java/com/github/litermc/vtil/mixin/valkyrienskies/MixinShipObjectServer.java
@@ -24,7 +24,7 @@ public class MixinShipObjectServer implements ShipObjectServerAccessor {
 		return this.serverTickListeners.values();
 	}
 
-	@Inject(method = "applyAttachmentInterfaces", at = @At("HEAD"))
+	@Inject(method = "applyAttachmentInterfaces", at = @At("HEAD"), remap = false)
 	private void applyAttachmentInterfaces(final Class<?> clazz, final Object value, final CallbackInfo ci) {
 		if (value == null) {
 			this.serverTickListeners.remove(clazz);
