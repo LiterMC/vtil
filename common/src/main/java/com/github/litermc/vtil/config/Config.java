efe0ff7afabafb202bca960eba4a94cf2b5e8b29
zyxkad <zyxkad@gmail.com>
2025-12-15 23:33:01 -0700

    add TODO tag

--- common/src/main/java/com/github/litermc/vtil/config/Config.java
+++ common/src/main/java/com/github/litermc/vtil/config/Config.java
@@ -2,10 +2,11 @@ package com.github.litermc.vtil.config;
 
 public final class Config {
 	/**
+	 * TODO
 	 * Not defined in the regular config file.
 	 * Enable unstable shadow mixins. This has to be true for all unstable features to work.
 	 */
-	public static boolean enableShadowMixins = false;
+	public static boolean enableShadowMixins = !false;
 
 	/**
 	 * Reuse deleted ship's chunks to assemble new ships when possible.
